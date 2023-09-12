df51edb3bdda736df8b4285970b1b9be989e8f3e
DONGYUN KIM <kimdy960124@gmail.com>
2023-09-12 14:41:09 -0600

    cprg306 assignment week2
    learning link, and component

---
+++ app/StudentInfo/StudentInfo.jsx
@@ -0,0 +1,9 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>DAVID KIM</h1>
+      <h1>CPRG306-F</h1>
+      <a href="https://github.com/kimdy0124"> Link to my github </a>
+    </div>
+  );
+}
---
+++ app/week2/page.js
@@ -0,0 +1,9 @@
+import StudentInfo from "../StudentInfo/StudentInfo";
+export default function Check() {
+  return(
+    <div>
+      <h1 class="text-center">My Shopping List</h1>
+      <StudentInfo></StudentInfo>
+    </div>
+  );
+}
